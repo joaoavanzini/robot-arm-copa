dc73a564df4554e2f0afebf234a8fb3f08833d3c
João Victor Palhares <36644804+vicpb@users.noreply.github.com>
2019-08-08 21:14:50 -0300

    Update teste2.ino

--- src/teste2.ino
+++ src/teste2.ino
@@ -1,8 +1,8 @@
 #include <VarSpeedServo.h>
 
 /*
-	x.attach(pino, angulominimo, algumomaximo)
-	x.slowmove (angulo, velocidade)
+  x.attach(pino, angulominimo, algumomaximo)
+  x.slowmove (angulo, velocidade)
 */
 
 
@@ -21,68 +21,48 @@ void setup() {
  arm_d.attach(5, 1, 180);
  arm_e.attach(6, 1, 180);
  arm_f.attach(7, 1, 180);
- arm_g.attach(8, 1, 180);
+ arm_g.attach(8, 1, 180); //esse pino não está aguentando
 }
  
 void loop() {
 
 
 /*
-	Posição inicial - bloco 1
+  Posição inicial - bloco 1
 */
-
- arm_a.slowmove(10, 20);
+/*
+ arm_a.slowmove(90, 10);
  delay(2000);
- arm_b.slowmove(10, 20);
- delay(2000);
- arm_c.slowmove(10, 20);
- delay(2000);
- arm_d.slowmove(10, 20);
- delay(2000);
- arm_e.slowmove(10, 20);
- delay(2000);
- arm_f.slowmove(10, 20);
- delay(2000);
- arm_g.slowmove(10, 20);
- delay(2000);
-
+ 
+ arm_b.slowmove(50, 20);
+ delay(200);
+ arm_c.slowmove(50, 20);
+ delay(200);
+ 
+ arm_d.slowmove(50, 20);
+ delay(200);
+ arm_e.slowmove(50, 20);
+ delay(200);
+ arm_f.slowmove(50, 20);
+ delay(200);
+ arm_g.slowmove(50, 20);
+ delay(200);
+*/
+ arm_a.slowmove(150, 10);
+ delay(200);
  /*
-	Posição - bloco 2
-
-
- arm_a.slowmove(0, 45);
- delay(30);
- arm_b.slowmove(0, 45);
- delay(30);
- arm_c.slowmove(0, 10);
- delay(30);
- arm_d.slowmove(0, 20);
- delay(30);
- arm_e.slowmove(0, 45);
- delay(30);
- arm_f.slowmove(0, 45);
- delay(30);
- arm_g.slowmove(0, 10);
- delay(30);
-
- /*
-	Posição inicial - bloco 3
-
-
- arm_a.slowmove(0, 45);
- delay(30);
- arm_b.slowmove(0, 45);
- delay(30);
- arm_c.slowmove(0, 10);
- delay(30);
- arm_d.slowmove(0, 20);
- delay(30);
- arm_e.slowmove(0, 45);
- delay(30);
- arm_f.slowmove(0, 45);
- delay(30);
- arm_g.slowmove(0, 10);
- delay(30);
+ arm_b.slowmove(80, 20);
+ delay(200);
+ arm_c.slowmove(80, 20);
+ delay(200);
+ arm_d.slowmove(80, 20);
+ delay(200);
+ arm_e.slowmove(80, 20);
+ delay(200);
+ arm_f.slowmove(80, 20);
+ delay(200);
+ arm_g.slowmove(80, 20);
+ delay(200);
 
 */
 
